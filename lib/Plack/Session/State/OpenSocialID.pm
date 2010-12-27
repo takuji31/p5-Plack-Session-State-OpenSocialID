c67dd58883871fb3e74c4f1c46f25f838c41239b
Nishibayashi Takuji <takuji@senchan.jp>
2010-12-27 18:06:09 +0900

    initial commit

---
+++ lib/Plack/Session/State/OpenSocialID.pm
@@ -0,0 +1,33 @@
+package Plack::Session::State::OpenSocialID;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Plack::Session::State::OpenSocialID -
+
+=head1 SYNOPSIS
+
+  use Plack::Session::State::OpenSocialID;
+
+=head1 DESCRIPTION
+
+Plack::Session::State::OpenSocialID is
+
+=head1 AUTHOR
+
+Nishibayashi Takuji E<lt>takuji {at} senchan.jpE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
